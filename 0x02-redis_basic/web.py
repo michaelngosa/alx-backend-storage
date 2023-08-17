590df1021884ba9febb97c14c42365eb01e45747
michaelngosa <ngosamichael@gmail.com>
2023-08-17 02:12:58 +0000

    implement a get_page function

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,34 +1,32 @@
 #!/usr/bin/env python3
+"""
+In this tasks, we will implement a get_page function
+(prototype: def get_page(url: str) -> str:). The core of the function
+is very simple. It uses the requests module to obtain the HTML
+content of a particular URL and returns it
+"""
 import requests
-import time
-from functools import wraps
-from typing import Dict
+import redis
 
-cache: Dict[str, str] = {}
+
+r = redis.Redis()
+
 
 def get_page(url: str) -> str:
-    if url in cache:
-        print(f"Retrieving from cache: {url}")
-        return cache[url]
+    """
+    Inside get_page track how many times a particular URL was
+    accessed in the key count:{url} and cache the result with
+    an expiration time of 10 seconds
+    """
+    html = r.get(url)
+    if html:
+        return html.decode('utf-8')
     else:
-        print(f"Retrieving from web: {url}")
         response = requests.get(url)
-        result = response.text
-        cache[url] = result
-        return result
+        html = response.text
+        r.set(url, html, ex=10)
+        return html
 
-def cache_with_expiration(expiration: int):
-    def decorator(func):
-        @wraps(func)
-        def wrapper(*args, **kwargs):
-            url = args[0]
-            key = f"count:{url}"
-            if key in cache:
-                count, timestamp = cache[key]
-                if time.time() - timestamp > expiration:
-                    result = func(*args, **kwargs)
-                    cache[key] = (count+1, time.time())
-                    return result
-                else:
-                    cache[key] = (count+1, timestamp)
-                    return
+
+if __name__ == "__main__":
+    get_page("http://slowwly.robertomurray.co.uk")
